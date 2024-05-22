fdddf714a29247f12220ca100b7032157aca1287
ninhcongluc <ninhconglucit@gmail.com>
2024-05-22 21:49:11 +0700

    Config router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { Link, Route, Routes } from "react-router-dom";
+import Login from "./components/Login";
+import Homepage from "./components/Homepage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        > 
-          Login page2
-        </a>
-      </header>
+    <div className="container">
+      <nav>
+        <ul>
+          <Link to="/homepage" class="list">
+            Homepage
+          </Link>
+        </ul>
+      </nav>
+
+      {/* Defining routes path and rendering components as element */}
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Homepage />} />
+      </Routes>
     </div>
   );
 }
