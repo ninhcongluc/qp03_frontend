439c53a006f9e4ce04b3ffc867c8a94f084584c0
Leen <phungthilinh@192.168.1.77>
2024-05-26 15:23:36 +0700

    Add profile page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Login from "./components/Login/Login";
 import Homepage from "./components/Homepage";
+import Profile from "./components/Profile/Profile";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/home" element={<Homepage />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </div>
   );
---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+function Profile() {
+  return (
+    <div>Profile page</div>
+  );
+}
+
+export default Profile;
