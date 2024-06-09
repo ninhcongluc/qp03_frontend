aa471a3aafc2ffc0d77ab0f0feaf085648a1a2a5
tuannah <125261220+tuannah@users.noreply.github.com>
2024-06-09 20:38:49 +0700

    update table managerManagement

--- src/pages/Admin/ManagerManagement.js
+++ src/pages/Admin/ManagerManagement.js
@@ -2,10 +2,11 @@ import React from "react";
 import LeftMenu from "../../components/LeftMenu/AdminMenu";
 import ManagerAccountTable from "../../components/Table/ManageAccountTable";
 import { Box } from "@mui/material";
+import "./ManagerManagement.css";
 
 const ManagerManagementPage = () => {
   return (
-    <div>
+    <div class="form_table">
       <LeftMenu />
       <Box
         sx={{
