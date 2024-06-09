94dc146343abe94aaceeb0406fe7577acdf7f332
tuannah <125261220+tuannah@users.noreply.github.com>
2024-06-09 13:45:46 +0700

    update screen Admin

--- src/components/LeftMenu/AdminMenu.js
+++ src/components/LeftMenu/AdminMenu.js
@@ -5,15 +5,13 @@ import {
   ListItem,
   ListItemIcon,
   ListItemText,
+  Avatar,
+  Stack
 } from "@mui/material";
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import Avatar from '@mui/material/Avatar';
-import Stack from '@mui/material/Stack';
-import { deepOrange } from '@mui/material/colors';
+import { deepOrange } from "@mui/material/colors";
 import "./AdminMenu.css";
-import Link from '@mui/material/Link';
-
 
 const LeftMenu = () => {
   const navigate = useNavigate();
@@ -29,35 +27,40 @@ const LeftMenu = () => {
   };
 
   return (
-    <div>
-
-      <Drawer variant="permanent" anchor="left">
-        <List>
-          <Link href="./profile.js">
-           <div class="Admin_Menu">
-            <Stack direction="row" spacing={2}>
-            <Avatar sx={{ bgcolor: deepOrange[500] }}>A</Avatar>
+    <Drawer
+      variant="permanent"
+      anchor="left"
+      sx={{
+        '& .MuiDrawer-paper': {
+          backgroundColor: '#ff981d', 
+          color: '#fff', 
+        },
+      }}
+    >
+      <List>
+        <div className="Admin_Menu">
+          <Stack direction="row" spacing={2}>
+            <Avatar alt="Admin" src="" sx={{ width: 50, height: 50 }} />
+            <h3>Admin</h3>
           </Stack>
-          </div>
-          </Link>
-         
           
-          <ListItem button onClick={handleManagerManagementClick}>
-            <ListItemIcon>
-              <DashboardOutlined />
-            </ListItemIcon>
-            <ListItemText primary="Manager Management" />
-          </ListItem>
+        </div>
 
-          <ListItem button onClick={handleLogoutClick}>
-            <ListItemIcon>
-              <ExitToAppOutlined />
-            </ListItemIcon>
-            <ListItemText primary="Logout" />
-          </ListItem>
-        </List>
-      </Drawer>
-    </div>
+        <ListItem button onClick={handleManagerManagementClick}>
+          <ListItemIcon>
+            <DashboardOutlined />
+          </ListItemIcon>
+          <ListItemText primary="Manager Management" />
+        </ListItem>
+
+        <ListItem button onClick={handleLogoutClick}>
+          <ListItemIcon>
+            <ExitToAppOutlined />
+          </ListItemIcon>
+          <ListItemText primary="Logout" />
+        </ListItem>
+      </List>
+    </Drawer>
   );
 };
 
