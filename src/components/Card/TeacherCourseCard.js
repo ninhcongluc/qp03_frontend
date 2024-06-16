d427ce836754615a3cf9c3a6d9a21e2fb47b5624
Linh <LinhPTHE175304@fpt.edu.vn>
2024-06-16 22:27:14 +0700

    teacherListCourse

--- src/components/Card/TeacherCourseCard.js
+++ src/components/Card/TeacherCourseCard.js
@@ -1,26 +1,23 @@
 import React from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { CardActionArea, Grid } from "@mui/material";
 
 const TeacherCourseCard = ({ course }) => (
-  <Card style={{ maxWidth: 400, height: "100%" }}>
+  <Card style={{ maxWidth: 500, height: "100%" }}>
     <CardActionArea>
       <Grid container>
-        <Grid item xs={4}>
-          <CardMedia
-            component="img"
-            height="100%"
-            image={course.image}
-            alt={`${course.code} course`}
-          />
-        </Grid>
-        <Grid item xs={8}>
+        <Grid item xs={15}>
           <CardContent>
             <Typography gutterBottom variant="h5" component="div">
               {course.code}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {course.description}
+            </Typography>
+            <Typography variant="body1" color="text.primary" gutterBottom>
+              Semester: <b>{course.semesterName}</b>
             </Typography>
             <Typography variant="body1" color="text.primary" gutterBottom>
               Start Date: <b>{course.startDate}</b>
@@ -28,6 +25,7 @@ const TeacherCourseCard = ({ course }) => (
             <Typography variant="body1" color="text.primary" gutterBottom>
               Students: <b>{course.studentCount}</b>
             </Typography>
+            
           </CardContent>
         </Grid>
       </Grid>
