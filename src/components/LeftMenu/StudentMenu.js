995cff7ae885e54dc1c56a6ce30527ea6816b3be
TunaTroy <datnthe172790@fpt.edu.vn>
2024-06-17 08:28:58 +0700

    Merge branch 'main' of https://github.com/ninhcongluc/qp03_frontend into feature/admin

--- src/components/LeftMenu/StudentMenu.js
+++ src/components/LeftMenu/StudentMenu.js
@@ -1,10 +1,10 @@
 import { DashboardOutlined, ExitToAppOutlined } from "@mui/icons-material";
 import {
-  Drawer,
-  List,
-  ListItem,
-  ListItemIcon,
-  ListItemText,
+    Drawer,
+    List,
+    ListItem,
+    ListItemIcon,
+    ListItemText,
 } from "@mui/material";
 import React from "react";
 import { useNavigate } from "react-router-dom";
@@ -15,45 +15,63 @@ import { yellow } from "@mui/material/colors";
 import "./StudentMenu.css";
 
 const StudentMenu = () => {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const handleCourseManagementClick = () => {
-    navigate("/student/course-management");
-  };
+    const handleCourseManagementClick = () => {
+        navigate("/student/course-management");
+    };
 
-  const handleLogoutClick = () => {
-    localStorage.removeItem("token");
-    localStorage.removeItem("user");
-    window.location.replace("/");
-  };
+    const handleLogoutClick = () => {
+        localStorage.removeItem("token");
+        localStorage.removeItem("user");
+        window.location.replace("/");
+    };
 
-  return (
-    <div>
-      <Drawer variant="permanent" anchor="left">
-        <List>
-          <div class="Student_Menu">
-            <Stack direction="row" spacing={2}>
-              <Avatar sx={{ bgcolor: yellow[500] }}>S</Avatar>
-            </Stack>
-          </div>
+    return ( <
+        div >
+        <
+        Drawer variant = "permanent"
+        anchor = "left" >
+        <
+        List >
+        <
+        div class = "Student_Menu" >
+        <
+        Stack direction = "row"
+        spacing = { 2 } >
+        <
+        Avatar sx = {
+            { bgcolor: yellow[500] } } > S < /Avatar> <
+        /Stack> <
+        /div>
 
-          <ListItem button onClick={handleCourseManagementClick}>
-            <ListItemIcon>
-              <DashboardOutlined />
-            </ListItemIcon>
-            <ListItemText primary="My Courses" />
-          </ListItem>
+        <
+        ListItem button onClick = { handleCourseManagementClick } >
+        <
+        ListItemIcon >
+        <
+        DashboardOutlined / >
+        <
+        /ListItemIcon> <
+        ListItemText primary = "My Courses" / >
+        <
+        /ListItem>
 
-          <ListItem button onClick={handleLogoutClick}>
-            <ListItemIcon>
-              <ExitToAppOutlined />
-            </ListItemIcon>
-            <ListItemText primary="Logout" />
-          </ListItem>
-        </List>
-      </Drawer>
-    </div>
-  );
+        <
+        ListItem button onClick = { handleLogoutClick } >
+        <
+        ListItemIcon >
+        <
+        ExitToAppOutlined / >
+        <
+        /ListItemIcon> <
+        ListItemText primary = "Logout" / >
+        <
+        /ListItem> <
+        /List> <
+        /Drawer> <
+        /div>
+    );
 };
 
 export default StudentMenu;
