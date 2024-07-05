060b2dbc82d8aeee715efec9e69581cf822a5e61
TunaTroy <datnthe172790@fpt.edu.vn>
2024-07-05 15:19:20 +0700

    Update QuizPage

--- src/pages/Student/StudentQuizPage.js
+++ src/pages/Student/StudentQuizPage.js
@@ -1,12 +1,15 @@
-import { Typography, Card, CardContent } from "@mui/material";
-import Container from "@mui/material/Container";
-import Grid from "@mui/material/Grid";
-import Pagination from "@mui/material/Pagination";
-import Stack from "@mui/material/Stack";
 import React, { useState, useEffect, useCallback } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import ApiInstance from "../../axios";
 import MenuComponent from "../../components/LeftMenu/Menu";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardActionArea from "@mui/material/CardActionArea";
+import Typography from "@mui/material/Typography";
 
 const StudentQuizPage = () => {
   const { classId } = useParams();
@@ -15,16 +18,17 @@ const StudentQuizPage = () => {
   const [totalItem, setTotalItem] = useState(0);
   const [quizzes, setQuizzes] = useState([]);
   const quizzesPerPage = 6;
+  const [courses, setCourses] = useState({ code: "" });
 
   const fetchQuizData = useCallback(
-    async (page, limit) => {
+    async (page, limit, searchTerm = "") => {
       try {
         const response = await ApiInstance.get(
-          `/student/course-management/class/${classId}?type=quizzes&page=${page}&limit=${limit}`
+          `/student/course-management/class/${classId}?type=quizzes&page=${page}&limit=${limit}&name=${searchTerm}`
         );
         const { data } = response.data;
         if (data && data.quizzes) {
-          setQuizzes(data.quizzes.sort((a, b) => new Date(a.startDate) - new Date(b.startDate)));
+          setQuizzes(response.data.data.quizzes);
           setTotalItem(data.total);
         }
       } catch (error) {
@@ -35,8 +39,24 @@ const StudentQuizPage = () => {
   );
 
   useEffect(() => {
-    fetchQuizData(page, quizzesPerPage);
-  }, [fetchQuizData, page, quizzesPerPage]);
+    fetchQuizData(1, quizzesPerPage);
+  }, [fetchQuizData, quizzesPerPage]);
+
+  useEffect(() => {
+    const fetchCourseData = async () => {
+      try {
+        const response = await ApiInstance.get(
+          `/course/student-courses/class/${classId}`
+        );
+        if (response.data.data) {
+          setCourses(response.data.data);
+        }
+      } catch (error) {
+        console.error("Error fetching course data:", error);
+      }
+    };
+    fetchCourseData(); 
+  }, [classId]);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -60,41 +80,36 @@ const StudentQuizPage = () => {
   return (
     <div>
       <MenuComponent role="student" />
+      <div className="tilte-class" style={{ marginLeft: "21%" }}>
+        <h2>
+          {courses.code}-{courses.name}
+        </h2>
+        <p>{courses.description}</p>
+      </div>
       <Container sx={{ marginLeft: "240px" }}>
-        <Grid container spacing={4} sx={{ marginTop: 2 }}>
-          <Grid item flex={1}>
-            <Typography variant="h4" component="h2">
-              NameCourse - CodeCourse   
-                description
-            </Typography>
-          </Grid>
-        </Grid>
         <Grid container spacing={4} sx={{ marginTop: 2, minHeight: 100 }}>
-          {quizzes
-            .slice(0, quizzesPerPage)
-            .map((quiz, index) => (
-              <Grid item key={index} xs={12} sm={6} md={4}>
-                <Card
-                  className="quiz-card"
-                  style={{ cursor: "pointer" }}
-                  onClick={() => handleQuizDetailClick(quiz.id)}
-                >
+          {quizzes.map((quiz, index) => (
+            <Grid item key={index} xs={12} sm={6} md={4}>
+              <Card onClick={() => handleQuizDetailClick(quiz.id)}>
+                <CardActionArea>
                   <CardContent>
-                    <Typography variant="h5" component="div">
+                    <Typography gutterBottom variant="h5" component="div">
                       {quiz.name}
                     </Typography>
-                    <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                    <Typography variant="body2" color="text.secondary">
                       {quiz.description}
                     </Typography>
-                    <Typography variant="body2">
+                    <Typography variant="body2" color="text.secondary">
                       Start Date: {new Date(quiz.startDate).toLocaleDateString()}
-                      <br />
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
                       End Date: {new Date(quiz.endDate).toLocaleDateString()}
                     </Typography>
                   </CardContent>
-                </Card>
-              </Grid>
-            ))}
+                </CardActionArea>
+              </Card>
+            </Grid>
+          ))}
         </Grid>
         <Stack spacing={2} sx={{ marginTop: 4, alignItems: "center" }}>
           <Pagination
