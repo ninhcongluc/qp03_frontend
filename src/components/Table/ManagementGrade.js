b57bc78cf48f45a8b1d8867bc7574b037297b056
ninhcongluc <ninhconglucit@gmail.com>
2024-07-22 23:27:12 +0700

    Update teacher page

--- src/components/Table/ManagementGrade.js
+++ src/components/Table/ManagementGrade.js
@@ -8,12 +8,26 @@ import {
   TableHead,
   TableRow,
   Select,
+  Button,
   MenuItem,
   InputLabel,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
 } from "@mui/material";
 import React, { useState, useEffect } from "react";
 import ApiInstance from "../../axios";
 import { formatDate } from "../../commons/function";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from "recharts";
 
 const ManagementGrade = () => {
   const userInfo = JSON.parse(localStorage.getItem("user"));
@@ -22,11 +36,11 @@ const ManagementGrade = () => {
   const [selectedClass, setSelectedClass] = useState("");
   const [selectedQuiz, setSelectedQuiz] = useState("");
   const [data, setData] = useState([]);
+  const [showStatisticsDialog, setShowStatisticsDialog] = useState(false);
 
   useEffect(() => {
     ApiInstance.get(`/class/list-by-teacher/${userInfo.id}`)
       .then((response) => {
-        console.log("25", response.data.data);
         setClassData(response.data.data);
       })
       .catch((error) => {
@@ -50,6 +64,7 @@ const ManagementGrade = () => {
     if (selectedClass !== "" && selectedQuiz !== "") {
       ApiInstance.get(`/quiz/${selectedQuiz}/student-grades`)
         .then((response) => {
+          console.log(response.data.data);
           setData(response.data.data);
         })
         .catch((error) => {
@@ -66,6 +81,31 @@ const ManagementGrade = () => {
   const handleQuizChange = (event) => {
     console.log(event.target.value);
     setSelectedQuiz(event.target.value);
+  };
+
+  const generateScoreDistribution = () => {
+    console.log(data);
+    const scoreDistribution = Array(11).fill(0); // Initialize an array with 11 zeros (0-10)
+
+    data.forEach((item) => {
+      const score = Math.floor(item.score);
+      scoreDistribution[score]++;
+    });
+
+    const rs = scoreDistribution.map((count, index) => ({
+      score: index,
+      students: count,
+    }));
+    console.log("rs", rs);
+    return rs;
+  };
+
+  const handleViewStatistic = () => {
+    setShowStatisticsDialog(true);
+  };
+
+  const handleCloseStatisticsDialog = () => {
+    setShowStatisticsDialog(false);
   };
 
   return (
@@ -163,6 +203,42 @@ const ManagementGrade = () => {
           <p>No Data</p>
         </Box>
       )}
+
+      <Box display="flex" justifyContent="flex-end" mb={2}>
+        {selectedClass && selectedQuiz && (
+          <Button
+            sx={{ width: 200, marginTop: 2 }}
+            variant="contained"
+            onClick={handleViewStatistic}
+          >
+            View Statistic
+          </Button>
+        )}
+      </Box>
+
+      <Dialog
+        open={showStatisticsDialog}
+        onClose={handleCloseStatisticsDialog}
+        maxWidth="xl"
+      >
+        <DialogTitle>Quiz Statistics</DialogTitle>
+        <DialogContent>
+          <BarChart width={800} height={400} data={generateScoreDistribution()}>
+            <XAxis dataKey="score" />
+            <YAxis type="number" domain={[0, "dataMax"]} tickCount={1} />
+
+            <CartesianGrid strokeDasharray="3 3" />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="students" fill="#8884d8" />
+          </BarChart>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseStatisticsDialog} color="primary">
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
     </Box>
   );
 };
