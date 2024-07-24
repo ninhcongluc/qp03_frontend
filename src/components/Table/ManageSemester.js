19c75f3a221dcf60bd00800e62df415b8df4846f
ninhcongluc <ninhconglucit@gmail.com>
2024-07-24 19:42:23 +0700

    List filter semester acitve

--- src/components/Table/ManageSemester.js
+++ src/components/Table/ManageSemester.js
@@ -40,7 +40,7 @@ const ManageSemesterTable = () => {
 
   const fetchSemesters = async () => {
     try {
-      const response = await ApiInstance.get("/semester?isActive=true");
+      const response = await ApiInstance.get("/semester");
       setSemesters(response.data.data);
     } catch (error) {
       console.error("Error fetching semester information:", error);
